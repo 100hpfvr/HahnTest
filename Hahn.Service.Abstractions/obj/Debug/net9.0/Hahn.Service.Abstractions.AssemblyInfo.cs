9f3675e583451f87c35a3ee9bd2d2b4fbded51cf
mateus <mateus.bellomo@prodatamobility.com.br>
2025-01-21 14:51:12 -0300

    Adicionar dependências do Entity Framework Core e ajustes

--- Hahn.Service.Abstractions/obj/Debug/net9.0/Hahn.Service.Abstractions.AssemblyInfo.cs
+++ Hahn.Service.Abstractions/obj/Debug/net9.0/Hahn.Service.Abstractions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hahn.Service.Abstractions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6214221ba74fb72c8ff7f6e7c9d04e85b2f8a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hahn.Service.Abstractions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hahn.Service.Abstractions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WorkerService/obj/Debug/net9.0/WorkerService.AssemblyInfo.cs
+++ WorkerService/obj/Debug/net9.0/WorkerService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6214221ba74fb72c8ff7f6e7c9d04e85b2f8a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
